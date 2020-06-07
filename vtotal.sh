fd4acfcda522f327e084a1f208902598b26b58b0
namunah <60642219+kernelrider@users.noreply.github.com>
2020-06-07 12:41:24 +0530

    Update vtotal.sh

--- vtotal.sh
+++ vtotal.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-printf "\n${RED}Getting Subdomain from Virustotal:${NC}\n"
-date +"%T"
+
 RED='\033[0;31m'
 NC='\033[0m'
+printf "\n${RED}Getting Subdomains from Virustotal:${NC}\n"
+date +"%T"
 read target
 curl --request GET --url "https://www.virustotal.com/api/v3/domains/$target/subdomains?limit=40" --header 'x-apikey: YOUR_API_KEY' | grep -o '"id": "[^"]*' | grep $target | cut -c 8- | tee -a "./virustotal.txt"
 curl --request GET --url "https://www.virustotal.com/api/v3/domains/$target/subdomains?cursor=STQwCi4%3D&limit=40" --header 'x-apikey: YOUR_API_KEY' | grep -o '"id": "[^"]*' | grep $target | cut -c 8- | tee -a "./virustotal.txt"
